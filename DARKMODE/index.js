7dfc93d8dd5fba0bbe4ac08da7c9a2030344bb50
Mariana Sausedo Zarate <44004376+Madis413@users.noreply.github.com>
2023-10-22 23:18:41 -0600

    Add files via upload

---
+++ DARKMODE/index.js
@@ -0,0 +1,8 @@
+function DARKMODE(){
+    /*Guardando en una variable a body para utilizarla después */
+    const body = document.body;
+    /*classList está en todos los elemnentos
+    y nos permite accedery manipular clases de CSS*/
+    body.classList.toggle("dark-mode");
+}
+
